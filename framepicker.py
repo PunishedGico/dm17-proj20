ecfc7db97a809bbd078eb16d48474611f9be6f27
PunishedGico <gicostudios@gmail.com>
2019-03-07 09:57:16 +0100

    Added comments

--- framepicker.py
+++ framepicker.py
@@ -1,21 +1,26 @@
 import cv2
 import numpy as np
 
+#Class that handles loading video, selecting frames,
+# and saving them as separate images in disk
 class Framepicker:
     def __init__(self):
         self.vidcap = None
-        self.imex = "jpg"
-        self.vidinfo = {}
-        self.metadata = []
+        self.imex = "jpg" #Extension of the images being saved
+        self.vidinfo = {} #Dictionary containing data about the video
+        self.metadata = [] #List of tuples marking sections to work on
 
+    #Destructor, probably useless and wrong?
     def __del__(self):
         self.vidcap.release()
 
+    #Dummy function for loading metadata
     def loadmetadata(self):
         for i in range(1):
             self.metadata.append((500, 1000))
             self.metadata.append((1500, 2000))
 
+    #Loads the video and stores the video info
     def loadvideo(self, filename):
         self.vidcap = cv2.VideoCapture(filename)
 
@@ -25,15 +30,18 @@ class Framepicker:
                 "height" : int(self.vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
                 "fps" : int(self.vidcap.get(cv2.CAP_PROP_FPS)),
                 "totalframes" : int(self.vidcap.get(cv2.CAP_PROP_FRAME_COUNT)),
-                "length" : int(self.vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+                "length" : int(self.vidcap.get(cv2.CAP_PROP_FRAME_COUNT)) #Calculate length of the video in seconds
                            / int(self.vidcap.get(cv2.CAP_PROP_FPS))
             }
             return True
 
         return False
 
+    #Picks frames from the video at intervals given
     def pickframes(self, interval):
+        #First loop through the number of sections specified in the metadata
         for section in range(len(self.metadata)):
+            #Then loop through each section with a step value of the interval given
             for i in range(self.metadata[section][0], self.metadata[section][1], interval):
                 self.vidcap.set(cv2.CAP_PROP_POS_FRAMES, i)
                 ret, frame = self.vidcap.read()
@@ -42,9 +50,11 @@ class Framepicker:
                     print("Saving frame: " + str(i))
                     self.saveframe(frame, "test" + str(i) + "." + self.imex)
 
+    #Dummy function for analysing a frame and determining if it's worth saving
     def analyseframe(self, frame):
         return True
 
+    #Saves a single frame to disk
     def saveframe(self, frame, filename):
         cv2.imwrite(filename, frame)
 
