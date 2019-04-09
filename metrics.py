3d492bf6907e47cba46a6b9c67e34ac30598b671
PunishedGico <gicostudios@gmail.com>
2019-04-09 13:10:38 +0200

    Updated visualisation code

--- metrics.py
+++ metrics.py
@@ -11,14 +11,22 @@ class Metrics:
         self.frames_with_detections = 0
 
     def add_detection(self, frame_num, det_classes):
+        self.total_frames += 1
+
+        detects = 0
+
+        if len(det_classes) > 0:
+            self.frames_with_detections += 1
+
         t = {"frame_num": frame_num}
         z = {**t, **det_classes}
         self.frames.append(z)
 
-    def visualise(self, interval):
-        img = np.zeros((40, 6648, 3), np.uint8)
-        cv2.rectangle(img, (0,0), (6648,40), (0,0,0,128), -1)
-
+    def visualise(self, interval, vidinfo):
+        tframes = vidinfo["totalframes"]
+        img = np.zeros((40, tframes, 3), np.uint8)
+        cv2.rectangle(img, (0,0), (tframes,40), (0,0,0,128), -1)
+        
         for frame in self.frames:
             for key, value in frame.items():
                 if "Bollard" in frame and frame["Bollard"] > 0:
@@ -33,75 +41,27 @@ class Metrics:
                 if "Rope" in frame and frame["Rope"] > 0:
                     cv2.rectangle(img, (frame["frame_num"],30), (frame["frame_num"]+interval,38), (239,255,254,255), -1)
 
-        asd = cv2.resize(img, (1920, 40))
+        asd = cv2.resize(img, (vidinfo["width"], 40))
+
+        r = self.frames_with_detections / self.total_frames
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(asd, "Relevance: " + str(r), (0,30), font, 0.5, (255,255,255))
+        
         return asd
     
-    def add_visualisation(self, filename, totalframes, interval):
-        vis = self.visualise(interval)
+    def add_visualisation(self, filename, interval, vidinfo):
+        vis = self.visualise(interval, vidinfo)
         
         for frame in self.frames:
             pos = frame["frame_num"]
-            asd = int((pos+(interval/2))/totalframes*1920)
+            tframes = vidinfo["totalframes"]
+            w = vidinfo["width"]
+            h = vidinfo["height"]
+
+            asd = int((pos+(interval/2))/tframes*w)
             f = filename + str(pos) + ".jpg"
             print("Adding visualisation to: " + f)
             img = cv2.imread(f)
-            img[1040:1040+40, 0:0+1920] = vis
-            cv2.line(img, (asd,1040), (asd,1080), (255,0,0), 1)
+            img[h-40:h, 0:0+w] = vis
+            cv2.line(img, (asd,h-40), (asd,h), (255,0,0), 1)
             cv2.imwrite(f, img)
-
-
-"""
-m = Metrics()
-
-
-m.add_detection(240, {"Bollard": 2})
-m.add_detection(480, {"Ladder": 1})
-m.add_detection(720, {"Bollard": 1, "Ladder": 4})
-
-m.visualise()
-
-print(m.frames)
-
-class Metrics:
-    def __init__(self):
-        self.frame = [{}]
-        self.classes = {}
-        self.total_detections = 0
-        self.frames_with_detections = 0
-        self.total_frames = 0
-
-    def __str__(self):
-        detection_rate = self.frames_with_detections / self.total_frames #Move
-        
-        ret = ""
-
-        for label in self.classes:
-            ret += str(label) + " | " + str(self.classes[label]) + " | " + str(self.classes[label] / self.total_frames) + "\n"
-
-        ret += "Frames with detections: " + str(self.frames_with_detections) + "\n"
-        ret += "Total detections: " + str(self.total_detections) + "\n"
-        ret += "Detection rate: " + str(detection_rate) + "\n"
-
-        return ret
-
-    def add(self, thing=None, inc=1):
-        if thing is not None:
-            self.total_detections += inc
-
-            if thing not in self.classes:
-                self.classes[thing] = inc
-            else:
-                self.classes[thing] += inc
-    
-    def merge(self, other):
-        self.total_frames += 1
-
-        if len(other.classes) > 0:
-            self.frames_with_detections += 1
-
-        for label in other.classes:
-            self.add(label, other.classes[label])
-
-    def update(self):
-        pass
-"""
